49fff394bab6eb7b7e64fceb855c1df822d20eec
Soham Tripathy <98731218+Tripathygithub@users.noreply.github.com>
2025-04-03 13:46:09 +0530

    Update admin.js

--- routes/v1/admin.js
+++ routes/v1/admin.js
@@ -6,6 +6,34 @@ var upload = multer({ storage: storage });
 
 
 ///////////////////////////////////////  epin ///////////////////////////////////////////////////
+const categoryController=require('../../controller/category');
+const subCategoryController=require('../../controller/subCategory');
+
+
+//category routes
+router.post('/category',categoryController.createCategory);
+
+router.get('/category',categoryController.getCategories);
+
+router.get('/category/:id',categoryController.getCategoryById);
+
+router.put('category/:id',categoryController.updateCategoryById);
+
+router.delete('/category/:id',categoryController.deleteCategoryById);
+
+// subCategory routes
+
+router.post('/sub-category',subCategoryController.createSubCategory);
+
+router.get('/sub-category',subCategoryController.getSubCategories);
+
+router.get('/sub-category/:id',subCategoryController.getSubCategoryById);
+
+router.put('/sub-category/:id',subCategoryController.updateSubCategoryById);
+
+router.delete('/sub-category/:id',subCategoryController.deleteSubCategoryById);
+
+
 
 
 module.exports = router;
